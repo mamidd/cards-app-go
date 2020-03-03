c6dc87b9eaf6fe7b56c75e00ea0fa1d4140bab0c
Mario <mamidd@gmai.com>
2020-03-03 20:56:49 +0100

    add final exercise

---
+++ compito/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	printEvenOddNumber(numbers)
+}
+
+func printEvenOddNumber(numbers []int) {
+	for _, number := range numbers {
+		if number%2 > 0 {
+			fmt.Println(strconv.Itoa(number) + " is even")
+		} else {
+			fmt.Println(strconv.Itoa(number) + " is odd")
+		}
+	}
+}
